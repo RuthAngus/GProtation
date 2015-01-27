fda86065c0956fcf4faf5a7dcb446015842afeb7
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-01-27 12:53:19 -0500

    getting there with interpolation

--- ACF.py
+++ ACF.py
@@ -19,36 +19,37 @@ def interpolate(x, y, yerr):
         m = l==False
         t_int = np.mean(diff[m])
 
-        # big arrays
-        xb = np.zeros(int((x[-1]-x[0])/t_int))
-        yb = np.zeros_like(xb)
-
         # these are the gaps
         n = np.where(diff > 2*mdiff)[0]
+
+        # big arrays
+        xb = x[:n[0]]
+        yb = y[:n[0]]
+
+        plt.clf()
+        plt.subplot(2, 1, 1)
+        plt.plot(x, y, "k.")
+
         for i in n:
-                xb[:i] = x[:i]  # fill in present values
-                yb[:i] = y[:i]  # fill in present values
+                xb = np.append(xb, x[i:i+1])
+                yb = np.append(yb, y[i:i+1])
 
                 # fill the gap
                 xgap = x[i:i+2]
                 ygap = y[i:i+2]
-
-                plt.clf()
-                plt.subplot(2, 1, 1)
-                plt.plot(xgap, ygap, "k.")
 
                 f = spi.interp1d(xgap, ygap)
                 xnew = np.arange(xgap[0], xgap[-1], t_int)
                 yerrnew = np.ones_like(xnew)*merr
                 ynew = f(xnew) + np.random.randn(len(xnew))*merr  # add noise
 
-                plt.subplot(2, 1, 2)
-                plt.plot(xnew, ynew, "k.")
-                plt.savefig("test")
-                assert 0
+                xb = np.append(xb, xnew)
+                yb = np.append(yb, ynew)
 
-                xb[i:i+len(xnew)] = xnew
-                yb[i:i+len(xnew)] = ynew
+        plt.subplot(2, 1, 2)
+        plt.plot(xb, yb, "k.")
+        plt.savefig("test")
+        raw_input('enter')
 
 # calculuate autocorrelation (assume evenly spaced)
 def acf(x, y):
@@ -57,7 +58,7 @@ def acf(x, y):
 
 if __name__ == "__main__":
 
-        DIR = "/n/home11/rangus/.kplr/data/lightcurves/010355856"
+        DIR = "/Users/angusr/.kplr/data/lightcurves/010355856"
         hdulist = pyfits.open("%s/kplr010355856-2013131215648_llc.fits" % DIR)
         tbdata = hdulist[1].data
         x = tbdata["TIME"]
