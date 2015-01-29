ab666d7a17e2d3cbba59f009338d6e2d8f7d0f40
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-01-29 13:45:29 -0500

    some changes

--- GProtation.py
+++ GProtation.py
@@ -7,6 +7,7 @@ import glob
 from ACF import load_fits
 import emcee
 import triangle
+import h5py
 
 from params import plot_params
 reb = plot_params()
@@ -16,28 +17,26 @@ cols = plot_colours()
 # take x, y, yerr, initial guess and fit an initial GP
 # prompt for initial guess
 # return GP params
-def basic(x, y, yerr, p_init, ID):
-
-    print(ID)
+def basic(x, y, yerr, p_init, ID, DIR):
 
     # initialise
-    theta_init = [1, 1, 1, p_init]
+    theta_init = [1, 1, 1, p_init, 1.]
     k = theta_init[0] * ExpSquaredKernel(theta_init[1]) \
             * ExpSine2Kernel(theta_init[2], theta_init[3])
     gp = george.GP(k)
-    gp.compute(x, yerr)
+    gp.compute(x, np.sqrt(theta[4]**2+yerr**2))
 
     # optimise
-    theta = gp.optimize(x, y, yerr)[0]
+    theta = gp.optimize(x, y, np.sqrt(theta[4]**2+yerr**2))[0]
 
     # predict
     xs = np.linspace(x[0], x[-1], 1000)
     mu, cov = gp.predict(y, xs)
 
     plt.clf()
-    plt.errorbar(x, y, yerr=yerr, **reb)
+    plt.errorbar(x, y, yerr=np.sqrt(theta[4]**2+yerr**2), **reb)
     plt.plot(xs, mu, color=cols.blue)
-    plt.savefig("%s_predict" % ID)
+    plt.savefig("%s/%s_predict" % (DIR, ID))
 
     return theta
 
@@ -55,7 +54,8 @@ def model(theta, x, y, yerr):
 # lnprior
 def lnprior(theta):
     if 1e-10 < theta[0] < 1e10 and 1e-10 < theta[1] < 1e10 and \
-            1e-10 < theta[2] < 1e10 and 0 < theta[3] < 100:
+            1e-10 < theta[2] < 1e10 and 0. < theta[3] < 100. and \
+            1e-10 < theta[4] < 1e10:
         return 0.0
     return -np.inf
 
@@ -74,8 +74,29 @@ def lnlike(theta, x, y, yerr):
         return 10e25
     return gp.lnlikelihood(y, quiet=True)
 
+# make various plots
+def make_plot(sampler, ID, DIR, traces=False):
+
+    nwalkers, nsteps, ndim = np.shape(sampler.chain)
+    flat = sampler.chain[:, 50:, :].reshape((-1, ndim))
+    mcmc_result = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
+                      zip(*np.percentile(flat, [16, 50, 84], axis=0)))
+    print(mcmc_result)
+
+    if traces:
+        print("Plotting traces")
+        for i in range(ndim):
+            plt.clf()
+            plt.plot(sampler.chain[:, :, i].T, 'k-', alpha=0.3)
+            plt.savefig("%s/%s.png" % (DIR, i))
+
+    print("Making triangle plot")
+    fig_labels = ["$A$", "$l_2$", "$l_1$", "$s$", "$P$", "$wn$"]
+    fig = triangle.corner(sampler.flatchain, labels=fig_labels)
+    fig.savefig("%s/%s_triangle.png" % (DIR, ID))
+
 # take x, y, yerr and initial guess and do MCMC
-def MCMC(theta_init, x, y, yerr, ID):
+def MCMC(theta_init, x, y, yerr, ID, DIR):
 
     ndim, nwalkers = len(theta_init), 32
     p0 = [theta_init+1e-4*np.random.rand(ndim) for i in range(nwalkers)]
@@ -85,28 +106,20 @@ def MCMC(theta_init, x, y, yerr, ID):
     print("burning in...")
     p0, lp, state = sampler.run_mcmc(p0, 100)
     sampler.reset()
-    p0, lp, state = sampler.run_mcmc(p0, 100)
+    print("production run...")
+    p0, lp, state = sampler.run_mcmc(p0, 1000)
 
-    flat = sampler.chain[:, 50:, :].reshape((-1, ndim))
-    mcmc_result = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
-                      zip(*np.percentile(flat, [16, 50, 84], axis=0)))
+    f = h5py.File("%s/%s_samples.h5" % (DIR, ID), "w")
+    data = f.create_dataset("samples", np.shape(sampler.chain))
+    data[:, :] = np.array(sampler.chain)
+    f.close()
 
-    print("Plotting traces")
-    for i in range(ndim):
-        plt.clf()
-        plt.axhline(theta_init[i], color = "r")
-        pl.plot(sampler.chain[:, :, i].T, 'k-', alpha=0.3)
-        pl.savefig("%s.png" % i)
-
-    print("Making triangle plot")
-    fig_labels = ["$A$", "$l_2$", "$l_1$", "$s$", "$P$"]
-    fig = triangle.corner(sampler.flatchain, truths=theta_init,
-                          labels=fig_labels)
-    fig.savefig("triangle.png")
+    return sampler
 
 if __name__ == "__main__":
 
-    D = "/Users/angusr/angusr/data2/Q15_public"
+    D = "/Users/angusr/angusr/data2/Q15_public"  # data directory
+    DIR = "/Users/angusr/Python/GProtation/mcmc"  # results directory
     fnames = glob.glob("%s/kplr0081*" % D)
 
     for fname in fnames:
@@ -125,12 +138,14 @@ if __name__ == "__main__":
         yerr /= med
 
         # try optimising
-#         p_init = float(raw_input("Enter initial guess for rotation period "))
-#         theta = basic(x, y, yerr, p_init, kid)
-#         print(theta)
-#         choose = raw_input("Use these parameters? y/n ")
-#         if choose == "y": theta = theta
-#         else: theta = [1., 1., 1., p_init]
 
-        theta = [1., 1., 1., 8., 1.]
-        MCMC(np.array(theta), x, y, yerr, kid)
+        p_init = float(raw_input("Enter initial guess for rotation period "))
+        theta = basic(x, y, yerr, p_init, kid, DIR)
+        print(theta)
+        choose = raw_input("Use these parameters? y/n ")
+        if choose == "y": theta = theta
+        else: theta = [1., 1., 1., p_init, 1.]
+
+#         theta = [1., 1., 1., 8., 1.]
+        sampler = MCMC(np.array(theta), x, y, yerr, kid, DIR)
+        make_plot(sampler, kid, DIR)
