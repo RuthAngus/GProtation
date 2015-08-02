7dc583b98070badcb8bd2218436634f52c8a0d50
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-08-01 19:13:57 -0700

    gisela

--- code/gisela.py
+++ code/gisela.py
@@ -5,10 +5,14 @@ from george.kernels import ExpSine2Kernel, ExpSquaredKernel
 import emcee
 import triangle
 import h5py
+from Kepler_ACF import corr_run
 
 class gisela(object)
 
-    def __init__(self):
+    def __init__(self, x, y, yerr):
+        self.x = x
+        self.y = y
+        self.yerr = yerr
 
     def lnprior(theta, plims):
         """
@@ -75,15 +79,25 @@ class gisela(object)
             return 10e25
         return -gp.lnlikelihood(y, quiet=True)
 
-    def MCMC(theta_init, x, y, yerr, plims, burnin, run)
+    def MCMC(theta_init, plims, burnin, run)
         """
         This function runs emcee in order to sample the posterior of your
         hyperparameters or find the maxima, etc.
 
+        theta_init (array): logarithmic initial parameters
+        [A, gamma, l, sigma, period]
+        A = amplitude.
+        gamma = scaling factor in the exponential-sine kernel.
+        l = length scale of the squared-exponential kernel
+        sigma = white noise multiplier
+        period = period (obv). In the same units as your 'xs'.
+
         It takes an initial guess for your parameters (theta_init).
         These must be logarithmic.
         It takes your data: x, y, yerr.
-        plims are your upper and lower prior bounds on the period.
+
+        plims are your (log) upper and lower prior bounds on the period.
+        same units as your x values.
         burnin (integer) is the number of steps during burn-in.
         run (integer) is the number of steps for the full run.
 
@@ -95,7 +109,7 @@ class gisela(object)
 
         ndim, nwalkers = len(theta_init), 32
         p0 = [theta_init+1e-4*np.random.rand(ndim) for i in range(nwalkers)]
-        args = (x, y, yerr, plims)
+        args = (self.x, self.y, self.yerr, plims)
         sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=args)
 
         print("burning in...")
@@ -103,78 +117,38 @@ class gisela(object)
         sampler.reset()
         print("production run...")
         p0, lp, state = sampler.run_mcmc(p0, run)
-
         return sampler
 
-    def bin_data(x, y, yerr, npts):
-    """
-    A function for binning your data.
-    Binning is sinning, of course, but if you want to get things
-    set up quickly this can be very helpful!
-    It takes your data: x, y, yerr
-    npts (int) is the number of points per bin.
-    """
-        mod, nbins = len(x) % npts, len(x) / npts
-        if mod != 0:
-            x, y, yerr = x[:-mod], y[:-mod], yerr[:-mod]
-        xb, yb, yerrb = [np.zeros(nbins) for i in range(3)]
-        for i in range(npts):
-            xb += x[::npts]
-            yb += y[::npts]
-            yerrb += yerr[::npts]**2
-            x, y, yerr = x[1:], y[1:], yerr[1:]
-        return xb/npts, yb/npts, yerrb**.5/npts
-
-    def fit(x, y, yerr, id, burnin=500, run=1500, npts=48, cutoff=100,
-            sine_kernel=False, acf=False, runMCMC=True,
-            plot=False):
+    def fit(id, theta_init=np.log([1., 1., 1., 1., 1.]), lower=.7, upper=1.5,
+            burnin=1000, run=1500):
         """
-        Takes x, y, yerr and initial guesses and priors for period, bins your
-        data sensibly, truncates it for speed and does the full GP MCMC.
-        x, y, yerr (arrays): your data.
-        id (str or int): the name of your object
-        p_init (tuple): upper and lower limits for your period prior
-        burnin (int): number of burnin steps
-        run (int): number of steps for the full run
-        npts (int): number of points per bin
+        Use this function if you want to remain agnostic about the
+        values of the less important hyperparameters and you want
+        to initialise with a period measured using the ACF method.
+
+        It usually seems ok to set the initial hyperparameter values to be
+        1.
+
+        id (str, int): the name of the star or whatever you want to save
+        the output as.
+
+        lower (float): the multiple of p_init you want to use for the lower
+        bound in the prior function.
+
+        upper (float): the multiple of p_init you want to use for the upper
+        bound in the prior function.
+
+        I set the upper and lower bounds on the prior to be .7 and
+        1.5 times the initial period.
         """
 
-        # measure ACF period
-        if acf:
-            corr_run(x, y, yerr, id, "/Users/angusr/Python/GProtation/code")
+        p_init, p_err = corr_run(self.x, self.y, self.yerr)
+        print "acf period, err = ", p_init
 
-        if sine_kernel:
-            print "sine kernel"
-            theta_init = [1., 1., 1., 1., p_init]
-            print theta_init
-            from GProtation import MCMC, make_plot
-        else:
-            print "cosine kernel"
-            theta_init = [1e-2, 1., 1e-2, p_init]
-            print "theta_init = ", np.log(theta_init)
-            from GProtation_cosine import MCMC, make_plot
-
-        xb, yb, yerrb = bin_data(x, y, yerr, npts)  # bin data
-        m = xb < cutoff  # truncate
-
-        theta_init = np.log(theta_init)
-        DIR = "cosine"
-        if sine_kernel:
-            DIR = "sine"
-
-        print theta_init
-        if runMCMC:
-            sampler = MCMC(theta_init, xb[m], yb[m], yerrb[m], plims, burnin, run,
-                           id, DIR)
-
-        # make various plots
-        if plot:
-            with h5py.File("%s/%s_samples.h5" % (DIR, str(int(id)).zfill(4)),
-                           "r") as f:
-                samples = f["samples"][...]
-            m = x < cutoff
-            mcmc_result = make_plot(samples, x[m], y[m], yerr[m], id, DIR,
-                                    traces=False, triangle=False, prediction=True)
+        # run MCMC
+        theta_init[-1] = np.log(p_init)
+        plims = np.log([p_init*lower, p_init*upper])
+        MCMC(id, theta_init, p_init, plims, burnin=burnin, run=run)
 
     # Dan Foreman-Mackey's acf function
     def dan_acf(x, axis=0, fast=False):
