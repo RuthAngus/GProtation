31524a5d8c274fb9cc2852c9aec90775ce517f60
RuthAngus <ruth.angus@astro.ox.ac.uk>
2015-12-30 17:39:14 +0000

    new prior - lengthscale longer than period

--- code/GProtation.py
+++ code/GProtation.py
@@ -18,8 +18,9 @@ def lnprior(theta, plims):
     """
     plims is a tuple, list or array containing the lower and upper limits for
     the rotation period. These are logarithmic!
+    theta = A, l, Gamma, s, P
     """
-    if -20 < theta[0] < 20 and -20 < theta[1] < 20 and -20 < theta[2] < 20 \
+    if -20 < theta[0] < 20 and period < theta[1] and -20 < theta[2] < 20 \
     and -20 < theta[3] < 20 and plims[0] < theta[4] < plims[1]:
         return 0.
     return -np.inf
