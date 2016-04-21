72986355abdc55c7c61a0ee1308ce51da8934ed6
RuthAngus <ruth.angus@astro.ox.ac.uk>
2016-04-21 20:16:32 +0000

    machinery working

--- ruth/recover_injections.py
+++ ruth/recover_injections.py
@@ -1,13 +1,14 @@
+from __future__ import print_function
 import numpy as np
 import matplotlib.pyplot as plt
-from GProtation import MCMC
+from GProtation import make_plot, lnprob
 from Kepler_ACF import corr_run
 import h5py
 from gatspy.periodic import LombScargle
 import sys
 import os
 import time
-from GProtation import make_plot, lnprob
+import emcee
 
 plotpar = {'axes.labelsize': 22,
            'font.size': 22,
@@ -35,7 +36,7 @@ def recover_injections(id):
         p_init, err = corr_run(x, y, yerr, id, "simulations")
         np.savetxt("simulations/{0}_acf_result.txt".format(id),
                    np.array([p_init, err]).T)
-    print "acf period, err = ", p_init, err
+    print("acf period, err = ", p_init, err)
 
     # Format data
     npts = 10
@@ -52,7 +53,10 @@ def recover_injections(id):
     cutoff = 10 * p_init
     theta_init = np.log([np.exp(-5), np.exp(7), np.exp(.6), np.exp(-16),
                         p_init])
-    burnin, run, nwalkers = 5, 50, 12
+    burnin, run, nwalkers = 50, 100, 12
+    ndim = len(theta_init)
+    p0 = [theta_init+1e-4*np.random.rand(ndim) for i in range(nwalkers)]
+    args = (xb, yb, yerrb, plims)
 
     # time the lhf call
     start = time.time()
@@ -62,12 +66,31 @@ def recover_injections(id):
     print("1 lhf call takes ", tm, "seconds")
     print("burn in will take", tm * nwalkers * burnin, "s")
     print("run will take", tm * nwalkers * run, "s")
-    print("total = ", (tm * nwalkers * run + tm * nwalkers * burnin)/60, \
+    print("total = ", (tm * nwalkers * run + tm * nwalkers * burnin)/60,
           "mins")
 
-    sampler = MCMC(theta_init, xb, yb, yerrb, plims, burnin, run, id,
-                   "simulations", nwalkers)
-    make_plot(sampler, xb, yb, yerrb, id, "simulations", traces=True, tri=True)
+    # run MCMC
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=args)
+    print("burning in...")
+    start = time.time()
+    p0, _, state = sampler.run_mcmc(p0, burnin)
+    sampler.reset()
+    print("production run...")
+    p0, _, state = sampler.run_mcmc(p0, run)
+    end = time.time()
+    print("actual time = ", end - start)
+
+    # save samples
+    f = h5py.File("simulations/%s_samples.h5" % (id), "w")
+    data = f.create_dataset("samples", np.shape(sampler.chain))
+    data[:, :] = np.array(sampler.chain)
+    f.close()
+
+    # make various plots
+    with h5py.File("simulations/%s_samples.h5" % (id), "r") as f:
+        samples = f["samples"][...]
+    mcmc_result = make_plot(samples, xb, yb, yerrb, id, "simulations",
+                            traces=True, tri=True, prediction=True)
 
 if __name__ == "__main__":
 
