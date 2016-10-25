423c0a9237999463d3ff1d3fbd2c136698247157
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2016-10-24 20:11:13 -0400

    adding comparison plot

--- gprotation/recover_suzannes.py
+++ gprotation/recover_suzannes.py
@@ -2,6 +2,7 @@ import numpy as np
 from GProt import calc_p_init, fit
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
 
 DATA_DIR = "../code/simulations/kepler_diffrot_full/final"
 RESULTS_DIR = "results/"
@@ -13,11 +14,35 @@ def load_suzanne_lcs(id):
                                       "lightcurve_{0}.txt".format(id))).T
     return x - x[0], y - 1
 
+
+def comparison_plot(truths):
+    """
+    Plot the acf, pgram and GP results.
+    """
+    m = truths.DELTA_OMEGA.values == 0
+    pgrams, acfs = [np.zeros_like(truths.N.values[m]) for i in range(2)]
+    pgram_errs, acf_errs = [np.zeros_like(truths.N.values[m]) for i in
+                            range(2)]
+    for i, id in enumerate(truths.N.values[m]):
+        acf_fname = os.path.join(RESULTS_DIR, "{0}_acf_result.txt".format(id))
+        acfs[i], acf_errs[i] = np.genfromtxt(acf_fname).T
+        p_fname = os.path.join(RESULTS_DIR, "{0}_pgram_result.txt".format(id))
+        pgrams[i], pgram_errs[i] = np.genfromtxt(p_fname).T
+
+    plt.clf()
+    plt.errobar(truths.P_MIN.values[m], acfs, yerr=acf_errs, fmt="k.",
+                capsize=0)
+    plt.plot(truths.P_MIN.values[m], pgrams, "r.")
+
+
 if __name__ == "__main__":
 
     DIR = "../code/simulations/kepler_diffrot_full/par/"
     truths = pd.read_csv(os.path.join(DIR, "final_table.txt"), delimiter=" ")
     m = truths.DELTA_OMEGA.values == 0
+
+    comparison_plot(truths)
+    assert 0
 
     for i, id in enumerate(truths.N.values[m]):  # zero diffrot
         print(id, i, "of", len(truths.N.values[m]))
