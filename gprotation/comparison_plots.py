cc2a9a72fed3cadacb9136a39ffe76f70e469d9a
RuthAngus <ruth.angus@astro.ox.ac.uk>
2016-10-28 16:14:51 +0000

    debugging loading from pandas dfs

--- gprotation/comparison_plots.py
+++ gprotation/comparison_plots.py
@@ -10,6 +10,49 @@ import h5py
 DATA_DIR = "../code/simulations/kepler_diffrot_full/final"
 RESULTS_DIR = "results/"
 
+
+def load_samples(id):
+    fname = os.path.join(RESULTS_DIR, "{0}.h5".format(str(int(id)).zfill(4)))
+    if os.path.exists(fname):
+        with h5py.File(fname, "r") as f:
+            samples = f["samples"][...]
+    nwalkers, nsteps, ndims = np.shape(samples)
+    return np.reshape(samples[:, :, 4], nwalkers * nsteps)
+
+
+def make_new_df(truths, R_DIR):
+    """
+    Load all the resulting period measurements and make a new pandas
+    dataframe.
+    """
+    m = truths.DELTA_OMEGA.values == 0
+
+    # get column names
+    mfname2 = os.path.join(R_DIR, "0002_mcmc_results.csv")
+    apfname2 = os.path.join(R_DIR, "0002_acf_pgram_results.csv")
+    mdf2, adf2 = pd.read_csv(mfname2), pd.read_csv(apfname2)
+
+    # assemble master data frame
+    mcols, acols = mdf2.columns.values, adf2.columns.values
+    mcmc = pd.DataFrame(data=np.zeros((0, len(mcols))), columns=mcols)
+    acf_pgram = pd.DataFrame(data=np.zeros((0, len(acols))), columns=acols)
+    Ns = []
+    for i, id in enumerate(truths.N.values[m]):
+        sid = str(int(id)).zfill(4)
+        mfname = os.path.join(R_DIR, "{0}_mcmc_results.csv".format(sid))
+        afname = os.path.join(R_DIR, "{0}_acf_pgram_results.csv".format(sid))
+        if os.path.exists(mfname) and os.path.exists(afname):
+            Ns.append(int(sid))
+            mcmc = pd.concat([mcmc, pd.read_csv(mfname)], axis=0)
+            acf_pgram = pd.concat([acf_pgram, pd.read_csv(afname)], axis=0)
+
+    mcmc["N"], acf_pgram["N"] = np.array(Ns), np.array(Ns)
+    truths1 = mcmc.merge(acf_pgram, on="N")
+    truths_s = truths.merge(truths1, on="N")
+    truths_s.to_csv("truths_extended.csv")
+    return truths_s
+
+
 def comparison_plot(truths, DIR):
     """
     Plot the acf, pgram and GP results.
@@ -32,12 +75,11 @@ def comparison_plot(truths, DIR):
     acfs = truths_e.acf_period.values[m]
     acf_errs = truths_e.acf_period_err.values[m]
     pgram = truths_e.pgram_period.values[m]
-    med = truths_e.med_mcmc_period.values[m]
-    med_errp = truths_e.med_mcmc_period_errp.values[m]
-    med_errm = truths_e.med_mcmc_period_errm.values[m]
-    maxlike = truths_e.maxlike_mcmc_period[m]
+    med = np.exp(truths_e.sigma.values[m])  # period and sigma names swapped
+    med_errp = np.exp(truths_e.sigma_errp.values[m])
+    med_errm = np.exp(truths_e.sigma_errm.values[m])
+    maxlike = np.exp(truths_e.sigma_max[m])
     amp = truths_e.AMP.values[m]
-    var = truths_e.variance.values[m]
 
     # mcmc plot
     plt.clf()
@@ -50,10 +92,10 @@ def comparison_plot(truths, DIR):
     plt.ylabel("$\mathrm{Recovered~Period~(Days)}$")
     plt.errorbar(true, maxlike, yerr=[med_errp, med_errm], fmt="k.", zorder=0,
                  capsize=0)
-    plt.scatter(true, maxlike, c=np.log(var), edgecolor="", cmap="GnBu",
-                vmin=min(np.log(var)), vmax=max(np.log(var)), s=50, zorder=1)
+    plt.scatter(true, maxlike, c=np.log(amp), edgecolor="", cmap="GnBu",
+                vmin=min(np.log(amp)), vmax=max(np.log(amp)), s=50, zorder=1)
     cbar = plt.colorbar()
-    cbar.ax.set_ylabel("$\ln\mathrm{(Variance)}$")
+    cbar.ax.set_ylabel("$\ln\mathrm{(Amplitude)}$")
     plt.savefig(os.path.join(DIR, "compare_mcmc"))
 
     # mcmc plot with samples
@@ -99,6 +141,8 @@ def comparison_plot(truths, DIR):
 
 if __name__ == "__main__":
 
-    truths = pd.read_csv("truths_extended.csv")
+    DIR = "../code/simulations/kepler_diffrot_full/par/"
+    truths = pd.read_csv(os.path.join(DIR, "final_table.txt"), delimiter=" ")
+
     comparison_plot(truths, "results_prior")
     comparison_plot(truths, "results")
