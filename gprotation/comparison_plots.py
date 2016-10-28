d6b7c36f5b5d91ec27d0742cb8fc3e7ee74e12e5
RuthAngus <ruth.angus@astro.ox.ac.uk>
2016-10-28 18:39:42 +0000

    separating plots into different functions

--- gprotation/comparison_plots.py
+++ gprotation/comparison_plots.py
@@ -7,6 +7,14 @@ import matplotlib.pyplot as plt
 from multiprocessing import Pool
 import h5py
 
+plotpar = {'axes.labelsize': 18,
+           'font.size': 10,
+           'legend.fontsize': 15,
+           'xtick.labelsize': 18,
+           'ytick.labelsize': 18,
+           'text.usetex': True}
+plt.rcParams.update(plotpar)
+
 DATA_DIR = "../code/simulations/kepler_diffrot_full/final"
 RESULTS_DIR = "results/"
 
@@ -53,18 +61,10 @@ def make_new_df(truths, R_DIR):
     return truths_s
 
 
-def comparison_plot(truths, DIR):
+def mcmc_plots(truths, DIR):
     """
-    Plot the acf, pgram and GP results.
+    Plot the GP results.
     """
-
-    plotpar = {'axes.labelsize': 18,
-               'font.size': 10,
-               'legend.fontsize': 15,
-               'xtick.labelsize': 18,
-               'ytick.labelsize': 18,
-               'text.usetex': True}
-    plt.rcParams.update(plotpar)
 
     truths_e = make_new_df(truths, DIR)
     m = (truths_e.DELTA_OMEGA.values == 0) \
@@ -72,9 +72,6 @@ def comparison_plot(truths, DIR):
 
     N = truths_e.N.values[m]
     true = truths_e.P_MIN.values[m]
-    acfs = truths_e.acf_period.values[m]
-    acf_errs = truths_e.acf_period_err.values[m]
-    pgram = truths_e.pgram_period.values[m]
     med = np.exp(truths_e.sigma.values[m])  # period and sigma names swapped
     med_errp = np.exp(truths_e.sigma_errp.values[m])
     med_errm = np.exp(truths_e.sigma_errm.values[m])
@@ -114,25 +111,61 @@ def comparison_plot(truths, DIR):
                  "k.", ms=1)
     plt.savefig(os.path.join(DIR, "compare_mcmc_samples"))
 
+
+def acf_plot(truths, DIR):
+    """
+    Plot the acf results.
+    """
+    truths_e = make_new_df(truths, DIR)
+    m = (truths_e.DELTA_OMEGA.values == 0) \
+            * (truths_e.acf_period.values > 0)
+
+    N = truths_e.N.values[m]
+    true = truths_e.P_MIN.values[m]
+    acfs = truths_e.acf_period.values[m]
+    acf_errs = truths_e.acf_period_err.values[m]
+    amp = truths_e.AMP.values[m]
+
     # acf plot
     plt.clf()
     xs = np.arange(0, 100, 1)
     plt.plot(xs, xs, "k--", alpha=.5)
     plt.plot(xs, 2*xs, "k--", alpha=.5)
     plt.errorbar(true, acfs, yerr=acf_errs, fmt="k.", capsize=0, ecolor=".7",
-                 alpha=.5)
+                 zorder=0)
+    plt.scatter(true, acfs, c=np.log(amp), edgecolor="", cmap="GnBu",
+                vmin=min(np.log(amp)), vmax=max(np.log(amp)), s=50, zorder=1)
+    cbar = plt.colorbar()
+    cbar.ax.set_ylabel("$\ln\mathrm{(Amplitude)}$")
     plt.ylim(0, 100)
     plt.xlim(0, 55)
     plt.xlabel("$\mathrm{Injected~Period~(Days)}$")
     plt.ylabel("$\mathrm{Recovered~Period~(Days)}$")
     plt.savefig(os.path.join(DIR, "compare_acf"))
 
+def pgram_plot(truths, DIR):
+    """
+    Plot the pgram results.
+    """
+
+    truths_e = make_new_df(truths, DIR)
+    m = (truths_e.DELTA_OMEGA.values == 0) \
+            * (truths_e.acf_period.values > 0)
+
+    N = truths_e.N.values[m]
+    true = truths_e.P_MIN.values[m]
+    pgram = truths_e.pgram_period.values[m]
+    amp = truths_e.AMP.values[m]
+
     # pgram plot
     plt.clf()
     xs = np.arange(0, 100, 1)
     plt.plot(xs, xs, "k--", alpha=.5)
     plt.plot(xs, 2*xs, "k--", alpha=.5)
-    plt.plot(true, pgram, "r.", alpha=.5)
+    plt.scatter(true, pgram, c=np.log(amp), edgecolor="", cmap="GnBu",
+                vmin=min(np.log(amp)), vmax=max(np.log(amp)), s=50, zorder=1)
+    cbar = plt.colorbar()
+    cbar.ax.set_ylabel("$\ln\mathrm{(Amplitude)}$")
     plt.ylim(0, 100)
     plt.xlim(0, 55)
     plt.xlabel("$\mathrm{Injected~Period~(Days)}$")
@@ -144,5 +177,7 @@ if __name__ == "__main__":
     DIR = "../code/simulations/kepler_diffrot_full/par/"
     truths = pd.read_csv(os.path.join(DIR, "final_table.txt"), delimiter=" ")
 
-    comparison_plot(truths, "results_prior")
-    comparison_plot(truths, "results")
+    mcmc_plots(truths, "results_prior")
+    mcmc_plots(truths, "results")
+    acf_plot(truths, "results")
+    pgram_plot(truths, "results")
