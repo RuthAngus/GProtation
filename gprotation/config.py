c4de3b3c79a70302c3761f5cc55cf0c2d3af92c1
Timothy Morton <tim.morton@gmail.com>
2016-11-04 12:54:32 -0400

    changed env variable name

--- gprotation/config.py
+++ gprotation/config.py
@@ -1,5 +1,5 @@
 import os
 
-AIGRAIN_DIR = os.getenv('AIGRAIN_DIR', 
+AIGRAIN_DIR = os.getenv('AIGRAIN_ROTATION', 
                         "../code/simulations/kepler_diffrot_full")
 POLYCHORD = os.getenv('POLYCHORD', os.path.expanduser('~/PolyChord'))
