2e92fd918fa3dde1e04ad39b85995f56e5f44e51
Timothy Morton <tim.morton@gmail.com>
2016-11-07 23:47:54 -0500

    added little extra room

--- gprot/summary.py
+++ gprot/summary.py
@@ -13,14 +13,14 @@ def corner_plot(basename, true_period=None, **kwargs):
     df = GPRotModel.get_mnest_samples(basename)
     if len(true_period)==2:
         P1, P2 = true_period
-        rng = [0.999, 0.999, 0.999, 0.999, (min(df['ln_period'].min(), P1),
-                                            max(df['ln_period'].max(), P2))]
+        rng = [0.999, 0.999, 0.999, 0.999, (min(df['ln_period'].min(), P1)-0.01,
+                                            max(df['ln_period'].max(), P2)+0.01)]
         fig = corner.corner(df, labels=GPRotModel.param_names, range=rng, **kwargs)
         axes = fig.get_axes()
         axes[-1].axvspan(true_period[0], true_period[1], color='g', alpha=0.3)
     else:
-        rng = [0.999, 0.999, 0.999, 0.999, (min(df['ln_period'].min(), true_period),
-                                            max(df['ln_period'].max(), true_period))]
+        rng = [0.999, 0.999, 0.999, 0.999, (min(df['ln_period'].min(), true_period)-0.01,
+                                            max(df['ln_period'].max(), true_period)+0.01)]
         truths = [None, None, None, None, true_period]
         fig = corner.corner(df, labels=GPRotModel.param_names, 
                         truths=truths, **kwargs)
