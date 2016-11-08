8c9a2883aa4d91651d8de0b2af9a9b92347de24e
Timothy Morton <tim.morton@gmail.com>
2016-11-07 23:46:34 -0500

    fixed errors

--- gprot/summary.py
+++ gprot/summary.py
@@ -13,14 +13,14 @@ def corner_plot(basename, true_period=None, **kwargs):
     df = GPRotModel.get_mnest_samples(basename)
     if len(true_period)==2:
         P1, P2 = true_period
-        rng = [0.999, 0.999, 0.999, 0.999, (min(df['period'].min(), P1),
-                                            max(df['period'].max(), P2))]
+        rng = [0.999, 0.999, 0.999, 0.999, (min(df['ln_period'].min(), P1),
+                                            max(df['ln_period'].max(), P2))]
         fig = corner.corner(df, labels=GPRotModel.param_names, range=rng, **kwargs)
         axes = fig.get_axes()
         axes[-1].axvspan(true_period[0], true_period[1], color='g', alpha=0.3)
     else:
-        rng = [0.999, 0.999, 0.999, 0.999, (min(true_period, P1),
-                                            max(true_period, P2))]
+        rng = [0.999, 0.999, 0.999, 0.999, (min(df['ln_period'].min(), true_period),
+                                            max(df['ln_period'].max(), true_period))]
         truths = [None, None, None, None, true_period]
         fig = corner.corner(df, labels=GPRotModel.param_names, 
                         truths=truths, **kwargs)
