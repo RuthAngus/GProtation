9a1ab76610d193672da863daf6f56b12f204016c
RuthAngus <ruth.angus@astro.ox.ac.uk>
2016-11-15 18:49:46 +0000

    log axes and looking at individual cases

--- gprotation/diagnostics.py
+++ gprotation/diagnostics.py
@@ -24,7 +24,7 @@ def make_new_df(truths, R_DIR):
 
     # get column names
     mfname2 = os.path.join(R_DIR, "0002_mcmc_results.txt")
-    apfname2 = os.path.join(R_DIR, "0002_acf_pgram_results.csv")
+    apfname2 = os.path.join(R_DIR, "0002_acf_pgram_results.txt")
     mdf2, adf2 = pd.read_csv(mfname2), pd.read_csv(apfname2)
 
     # assemble master data frame
@@ -35,7 +35,7 @@ def make_new_df(truths, R_DIR):
     for i, id in enumerate(truths.N.values[m]):
         sid = str(int(id)).zfill(4)
         mfname = os.path.join(R_DIR, "{0}_mcmc_results.txt".format(sid))
-        afname = os.path.join(R_DIR, "{0}_acf_pgram_results.csv".format(sid))
+        afname = os.path.join(R_DIR, "{0}_acf_pgram_results.txt".format(sid))
         if os.path.exists(mfname) and os.path.exists(afname):
             Ns.append(int(sid))
             mcmc = pd.concat([mcmc, pd.read_csv(mfname)], axis=0)
@@ -79,145 +79,159 @@ def plots(truths, DIR):
     # plot mcmc results for acf successes
     plt.clf()
     xs = np.arange(0, 100, 1)
-    plt.plot(xs, xs, "k--", alpha=.5)
-    plt.plot(xs, 2*xs, "k--", alpha=.5)
-    plt.plot(xs, .5*xs, "k--", alpha=.5)
-    plt.ylim(0, 100)
-    plt.xlim(0, 55)
+    plt.plot(np.log(xs), np.log(xs), "k--", alpha=.5)
+    plt.plot(np.log(xs), np.log(xs) + 2, "k--", alpha=.5)
+    plt.plot(np.log(xs), np.log(xs) - 2, "k--", alpha=.5)
+    plt.xlim(0, 4)
+    plt.ylim(0, 6)
     plt.title("$\mathrm{MCMC~results~for~ACF~successes}$")
-    plt.xlabel("$\mathrm{Injected~Period~(Days)}$")
-    plt.ylabel("$\mathrm{Recovered~Period~(Days)}$")
-    plt.scatter(true[ma], maxlike[ma], c=np.log(amp[ma]), edgecolor="",
-                cmap="GnBu",
+    plt.xlabel("$\ln(\mathrm{Injected~Period})$")
+    plt.ylabel("$\ln(\mathrm{Recovered~Period})$")
+    plt.scatter(np.log(true[ma]), np.log(maxlike[ma]), c=np.log(amp[ma]),
+                edgecolor="", cmap="GnBu",
                 vmin=min(np.log(amp)), vmax=max(np.log(amp)), s=50, zorder=1)
     cbar = plt.colorbar()
     cbar.ax.set_ylabel("$\ln\mathrm{(Amplitude)}$")
-    plt.savefig(os.path.join(DIR, "mcmc_acf"))
+    plt.savefig(os.path.join(DIR, "mcmc_acf.pdf"))
 
     # plot mcmc results for acf failures
     plt.clf()
     xs = np.arange(0, 100, 1)
-    plt.plot(xs, xs, "k--", alpha=.5)
-    plt.plot(xs, 2*xs, "k--", alpha=.5)
-    plt.ylim(0, 100)
-    plt.xlim(0, 55)
+    plt.plot(np.log(xs), np.log(xs), "k--", alpha=.5)
+    plt.plot(np.log(xs), np.log(xs) + 2, "k--", alpha=.5)
+    plt.plot(np.log(xs), np.log(xs) - 2, "k--", alpha=.5)
+    plt.xlim(0, 4)
+    plt.ylim(0, 6)
     plt.title("$\mathrm{MCMC~results~for~ACF~failures}$")
-    plt.xlabel("$\mathrm{Injected~Period~(Days)}$")
-    plt.ylabel("$\mathrm{Recovered~Period~(Days)}$")
-    plt.scatter(true[maf], maxlike[maf], c=np.log(amp[maf]), edgecolor="",
-                cmap="GnBu",
+    plt.xlabel("$\ln(\mathrm{Injected~Period})$")
+    plt.ylabel("$\ln(\mathrm{Recovered~Period})$")
+    plt.scatter(np.log(true[maf]), np.log(maxlike[maf]), c=np.log(amp[maf]),
+                edgecolor="", cmap="GnBu",
                 vmin=min(np.log(amp)), vmax=max(np.log(amp)), s=50, zorder=1)
     cbar = plt.colorbar()
     cbar.ax.set_ylabel("$\ln\mathrm{(Amplitude)}$")
-    plt.savefig(os.path.join(DIR, "mcmc_acf_fail"))
+    plt.savefig(os.path.join(DIR, "mcmc_acf_fail.pdf"))
 
     plt.clf()
     xs = np.arange(0, 100, 1)
-    plt.plot(xs, xs, "k--", alpha=.5)
-    plt.plot(xs, 2*xs, "k--", alpha=.5)
-    plt.ylim(0, 100)
-    plt.xlim(0, 55)
+    plt.plot(np.log(xs), np.log(xs), "k--", alpha=.5)
+    plt.plot(np.log(xs), np.log(xs) + 2, "k--", alpha=.5)
+    plt.plot(np.log(xs), np.log(xs) - 2, "k--", alpha=.5)
+    plt.xlim(0, 4)
+    plt.ylim(0, 6)
+    plt.xlabel("$\ln(\mathrm{Injected~Period})$")
+    plt.ylabel("$\ln(\mathrm{Recovered~Period})$")
     plt.title("$\mathrm{ACF~successes}$")
-    plt.xlabel("$\mathrm{Injected~Period~(Days)}$")
-    plt.ylabel("$\mathrm{Recovered~Period~(Days)}$")
-    plt.scatter(true[ma], acfs[ma], c=np.log(amp[ma]), edgecolor="",
-                cmap="GnBu",
+    plt.scatter(np.log(true[ma]), np.log(acfs[ma]), c=np.log(amp[ma]),
+                edgecolor="", cmap="GnBu",
                 vmin=min(np.log(amp)), vmax=max(np.log(amp)), s=50, zorder=1)
     cbar = plt.colorbar()
     cbar.ax.set_ylabel("$\ln\mathrm{(Amplitude)}$")
-    plt.savefig(os.path.join(DIR, "acf"))
+    plt.savefig(os.path.join(DIR, "acf.pdf"))
 
     plt.clf()
     xs = np.arange(0, 100, 1)
-    plt.plot(xs, xs, "k--", alpha=.5)
-    plt.plot(xs, 2*xs, "k--", alpha=.5)
-    plt.ylim(0, 100)
-    plt.xlim(0, 55)
+    plt.plot(np.log(xs), np.log(xs), "k--", alpha=.5)
+    plt.plot(np.log(xs), np.log(xs) + 2, "k--", alpha=.5)
+    plt.plot(np.log(xs), np.log(xs) - 2, "k--", alpha=.5)
+    plt.xlim(0, 4)
+    plt.ylim(0, 6)
+    plt.xlabel("$\ln(\mathrm{Injected~Period})$")
+    plt.ylabel("$\ln(\mathrm{Recovered~Period})$")
     plt.title("$\mathrm{ACF~failures}$")
-    plt.xlabel("$\mathrm{Injected~Period~(Days)}$")
-    plt.ylabel("$\mathrm{Recovered~Period~(Days)}$")
-    plt.scatter(true[maf], acfs[maf], c=np.log(amp[maf]), edgecolor="",
-                cmap="GnBu",
+    plt.scatter(np.log(true[maf]), np.log(acfs[maf]), c=np.log(amp[maf]),
+                edgecolor="", cmap="GnBu",
                 vmin=min(np.log(amp)), vmax=max(np.log(amp)), s=50, zorder=1)
     cbar = plt.colorbar()
     cbar.ax.set_ylabel("$\ln\mathrm{(Amplitude)}$")
-    plt.savefig(os.path.join(DIR, "acf_fail"))
+    plt.savefig(os.path.join(DIR, "acf_fail.pdf"))
 
     # plot acf results for mcmc successes
     plt.clf()
-    xs = np.arange(0, 100, 1)
-    plt.plot(xs, xs, "k--", alpha=.5)
-    plt.plot(xs, 2*xs, "k--", alpha=.5)
-    plt.ylim(0, 100)
-    plt.xlim(0, 55)
-    plt.xlabel("$\mathrm{Injected~Period~(Days)}$")
-    plt.ylabel("$\mathrm{Recovered~Period~(Days)}$")
+    plt.plot(np.log(xs), np.log(xs), "k--", alpha=.5)
+    plt.plot(np.log(xs), np.log(xs) + 2, "k--", alpha=.5)
+    plt.plot(np.log(xs), np.log(xs) - 2, "k--", alpha=.5)
+    plt.xlim(0, 4)
+    plt.ylim(0, 6)
+    plt.xlabel("$\ln(\mathrm{Injected~Period})$")
+    plt.ylabel("$\ln(\mathrm{Recovered~Period})$")
     plt.title("$\mathrm{ACF~results~for~MCMC~successes}$")
-    plt.scatter(true[mgp], acfs[mgp], c=np.log(amp[mgp]), edgecolor="",
-                cmap="GnBu",
+    plt.scatter(np.log(true[mgp]), np.log(acfs[mgp]), c=np.log(amp[mgp]),
+                edgecolor="", cmap="GnBu",
                 vmin=min(np.log(amp)), vmax=max(np.log(amp)), s=50, zorder=1)
     cbar = plt.colorbar()
     cbar.ax.set_ylabel("$\ln\mathrm{(Amplitude)}$")
-    plt.savefig(os.path.join(DIR, "acf_mcmc"))
+    plt.savefig(os.path.join(DIR, "acf_mcmc.pdf"))
 
     # plot acf results for mcmc failures
     plt.clf()
     xs = np.arange(0, 100, 1)
-    plt.plot(xs, xs, "k--", alpha=.5)
-    plt.plot(xs, 2*xs, "k--", alpha=.5)
-    plt.ylim(0, 100)
-    plt.xlim(0, 55)
-    plt.xlabel("$\mathrm{Injected~Period~(Days)}$")
-    plt.ylabel("$\mathrm{Recovered~Period~(Days)}$")
+    plt.plot(np.log(xs), np.log(xs), "k--", alpha=.5)
+    plt.plot(np.log(xs), np.log(xs) + 2, "k--", alpha=.5)
+    plt.plot(np.log(xs), np.log(xs) - 2, "k--", alpha=.5)
+    plt.xlim(0, 4)
+    plt.ylim(0, 6)
+    plt.xlabel("$\ln(\mathrm{Injected~Period})$")
+    plt.ylabel("$\ln(\mathrm{Recovered~Period})$")
     plt.title("$\mathrm{ACF~results~for~MCMC~failures}$")
-    plt.scatter(true[mgpf], acfs[mgpf], c=np.log(amp[mgpf]), edgecolor="",
-                cmap="GnBu",
+    plt.scatter(np.log(true[mgpf]), np.log(acfs[mgpf]), c=np.log(amp[mgpf]),
+                edgecolor="", cmap="GnBu",
                 vmin=min(np.log(amp)), vmax=max(np.log(amp)), s=50, zorder=1)
     cbar = plt.colorbar()
     cbar.ax.set_ylabel("$\ln\mathrm{(Amplitude)}$")
     plt.plot(true[N == 44], acfs[N == 44], "r.")
-    plt.savefig(os.path.join(DIR, "acf_mcmc_fail"))
+    plt.savefig(os.path.join(DIR, "acf_mcmc_fail.pdf"))
 
     plt.clf()
     xs = np.arange(0, 100, 1)
-    plt.plot(xs, xs, "k--", alpha=.5)
-    plt.plot(xs, 2*xs, "k--", alpha=.5)
-    plt.ylim(0, 100)
-    plt.xlim(0, 55)
+    plt.plot(np.log(xs), np.log(xs), "k--", alpha=.5)
+    plt.plot(np.log(xs), np.log(xs) + 2, "k--", alpha=.5)
+    plt.plot(np.log(xs), np.log(xs) - 2, "k--", alpha=.5)
+    plt.xlim(0, 4)
+    plt.ylim(0, 6)
+    plt.xlabel("$\ln(\mathrm{Injected~Period})$")
+    plt.ylabel("$\ln(\mathrm{Recovered~Period})$")
     plt.title("$\mathrm{MCMC~successes}$")
-    plt.xlabel("$\mathrm{Injected~Period~(Days)}$")
-    plt.ylabel("$\mathrm{Recovered~Period~(Days)}$")
-    plt.scatter(true[mgp], maxlike[mgp], c=np.log(amp[mgp]), edgecolor="",
-                cmap="GnBu",
+    plt.scatter(np.log(true[mgp]), np.log(maxlike[mgp]), c=np.log(amp[mgp]),
+                edgecolor="", cmap="GnBu",
                 vmin=min(np.log(amp)), vmax=max(np.log(amp)), s=50, zorder=1)
     cbar = plt.colorbar()
     cbar.ax.set_ylabel("$\ln\mathrm{(Amplitude)}$")
-    plt.savefig(os.path.join(DIR, "mcmc"))
+    plt.savefig(os.path.join(DIR, "mcmc.pdf"))
 
     plt.clf()
     xs = np.arange(0, 100, 1)
-    plt.plot(xs, xs, "k-", alpha=.5)
-    plt.plot(xs, 2*xs, "k--", alpha=.5)
-    plt.plot(xs, .5*xs, "k--", alpha=.5)
-    plt.ylim(0, 100)
-    plt.xlim(0, 55)
+    plt.plot(np.log(xs), np.log(xs), "k--", alpha=.5)
+    plt.plot(np.log(xs), np.log(xs) + 2, "k--", alpha=.5)
+    plt.plot(np.log(xs), np.log(xs) - 2, "k--", alpha=.5)
+    plt.xlim(0, 4)
+    plt.ylim(0, 6)
     plt.title("$\mathrm{MCMC~failures}$")
-    plt.xlabel("$\mathrm{Injected~Period~(Days)}$")
-    plt.ylabel("$\mathrm{Recovered~Period~(Days)}$")
-    plt.scatter(true[mgpf], maxlike[mgpf], c=np.log(amp[mgpf]), edgecolor="",
-                cmap="GnBu",
+    plt.xlabel("$\ln(\mathrm{Injected~Period})$")
+    plt.ylabel("$\ln(\mathrm{Recovered~Period})$")
+    plt.scatter(np.log(true[mgpf]), np.log(maxlike[mgpf]), c=np.log(amp[mgpf]),
+                edgecolor="", cmap="GnBu",
                 vmin=min(np.log(amp)), vmax=max(np.log(amp)), s=50, zorder=1)
     cbar = plt.colorbar()
     cbar.ax.set_ylabel("$\ln\mathrm{(Amplitude)}$")
     m = ((maxlike[mgpf] / true[mgpf]) > 1.3)
-#     * (maxlike[mgpf] > 40) \
-#             * (maxlike[mgpf] < 60)
-    print(N[mgpf][m])
-    num = 3
-    plt.plot(true[mgpf][m][num], maxlike[mgpf][m][num], "r.")
-    print(N[mgpf][m][num])
-    print(true[mgpf][m][num], maxlike[mgpf][m][num], acfs[mgpf][m][num])
-    plt.savefig(os.path.join(DIR, "mcmc_fail"))
+    mu = ((maxlike[mgpf] / true[mgpf]) < .5)
+    print("Overestimates: ", N[mgpf][m])
+    print("Underestimates: ", N[mgpf][mu])
+    print("Overestimations")
+    for num, over in enumerate(N[mgpf][m]):
+        print("ID: ", N[mgpf][m][num], ", true period = ", true[mgpf][m][num],
+              ", GP period = ", maxlike[mgpf][m][num], ", ACF period = ",
+              acfs[mgpf][m][num])
+    print("Underestimations")
+    for num, under in enumerate(N[mgpf][mu]):
+        print("ID: ", N[mgpf][mu][num], ", true period = ",
+              true[mgpf][mu][num], ", GP period = ", maxlike[mgpf][mu][num],
+              ", ACF period = ", acfs[mgpf][mu][num])
+    num = 2
+    plt.plot(np.log(true[mgpf][m][num]), np.log(maxlike[mgpf][m][num]), "r.")
+    plt.plot(np.log(true[mgpf][mu][num]), np.log(maxlike[mgpf][m][num]), "r.")
+    plt.savefig(os.path.join(DIR, "mcmc_fail.pdf"))
 
     plt.clf()
     plt.hist(gammas[mgpf], color="w", alpha=.5, histtype="stepfilled",
@@ -229,7 +243,7 @@ def plots(truths, DIR):
                                         np.std(gammas[mgp])))
     plt.legend()
     plt.xlabel("$\ln(\Gamma)$")
-    plt.savefig("gamma_comp")
+    plt.savefig(os.path.join(DIR, "gamma_comp.pdf"))
 
     plt.clf()
     plt.hist(ls[mgpf], color="w", alpha=.5, histtype="stepfilled",
@@ -241,7 +255,7 @@ def plots(truths, DIR):
                                         np.std(ls[mgp])))
     plt.legend()
     plt.xlabel("$\ln(l)$")
-    plt.savefig("l_comp")
+    plt.savefig(os.path.join(DIR, "l_comp.pdf"))
 
     plt.clf()
     plt.hist(As[mgpf], color="w", alpha=.5, histtype="stepfilled",
@@ -253,7 +267,7 @@ def plots(truths, DIR):
                                         np.std(As[mgp])))
     plt.legend()
     plt.xlabel("$\ln(A)$")
-    plt.savefig("A_comp")
+    plt.savefig(os.path.join(DIR, "A_comp.pdf"))
 
     plt.clf()
     plt.hist(ss[mgpf], color="w", alpha=.5, histtype="stepfilled",
@@ -265,7 +279,7 @@ def plots(truths, DIR):
                                         np.std(ss[mgp])))
     plt.legend()
     plt.xlabel("$\ln(\sigma)$")
-    plt.savefig("s_comp")
+    plt.savefig(os.path.join(DIR, "s_comp.pdf"))
 
     plt.clf()
     m = maxlike[mgpf] < 1000
@@ -278,7 +292,7 @@ def plots(truths, DIR):
                                         np.std(maxlike[mgp])))
     plt.legend()
     plt.xlabel("$\ln(\mathrm{Period})$")
-    plt.savefig("period_comp")
+    plt.savefig(os.path.join(DIR, "period_comp.pdf"))
 
 
 if __name__ == "__main__":
