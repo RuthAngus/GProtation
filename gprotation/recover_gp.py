0be3880dd7793c869dfc8ebe9e77f6fa5e83a2a7
RuthAngus <ruth.angus@astro.ox.ac.uk>
2016-11-20 17:15:25 +0000

    updating to latest version

--- gprotation/recover_gp.py
+++ gprotation/recover_gp.py
@@ -8,6 +8,7 @@ from multiprocessing import Pool
 import h5py
 from Kepler_ACF import corr_run
 from gatspy.periodic import LombScargle
+from recover_suzannes import make_lists, make_gaps
 
 def load_suzanne_lcs(id):
     sid = str(int(id)).zfill(4)
@@ -47,13 +48,18 @@ def recover(i):
     x_n, y_n = np.genfromtxt(os.path.join(DATA_DIR,
                              "{0}_noise.txt".format(sid))).T
     yerr_n = np.ones_like(y_n) * 1e-5
-    plt.clf()
-    plt.plot(x, y, "k.")
-    plt.savefig("test")
 
     # sigma clip
     x, y, yerr = sigma_clip(x, y, yerr, 5)
     x_n, y_n, yerr_n = sigma_clip(x_n, y_n, yerr_n, 5)
+
+    ppd = 4  # cut off at 200 days, 4 points per day
+    xb, yb, yerrb = make_gaps(x, y, yerr, ppd)
+    xb_n, yb_n, yerrb_n = make_gaps(x_n, y_n, yerr_n, ppd)
+
+    # make data into a list of lists, 200 days each
+    xb, yb, yerrb = make_lists(xb, yb, yerrb, 200)
+    xb_n, yb_n, yerrb_n = make_lists(xb_n, yb_n, yerrb_n, 200)
 
     # find p_init
     acf_period_n, a_err_n, pgram_period_n, p_err_n = \
@@ -67,33 +73,38 @@ def recover(i):
     p_init = acf_period
     p_init_n = acf_period_n
 
-    if p_init < .5:
-        p_init = 20.
-    if p_init_n < .5:
-        p_init_n = 20.
-    burnin, nwalkers, nruns, full_run = 1000, 16, 20, 1000
-    print("p_init = ", p_init, "p_init_n = ", p_init_n)
+    p_max = np.log((xb[0][-1] - xb[0][0]) / 2.)
+    if p_init > np.exp(p_max):
+        p_init = 40
+    elif p_init < .5:
+        p_init = 10
+    burnin, nwalkers, nruns, full_run = 1000, 16, 5, 1000
+
+    assert p_init < np.exp(p_max), "p_init > p_max"
 
     # fast settings:
-#     burnin, nwalkers, nruns, full_run = 2, 12, 2, 50
-#     x, y, yerr = x[::100], y[::100], yerr[::100]
-#     x_n, y_n, yerr_n = x_n[::100], y_n[::100], yerr_n[::100]
-
-    # set prior bounds
-    plims = np.log([.5*p_init, 1.5*p_init])
-    plims_n = np.log([.5*p_init_n, 1.5*p_init_n])
+    burnin, nwalkers, nruns, full_run = 2, 12, 5, 50
+    xb[0], yb[0], yerrb[0] = xb[0][::1000], yb[0][::1000], yerrb[0][::1000]
+    xb_n[0], yb_n[0], yerrb_n[0] = xb_n[0][::1000], yb_n[0][::1000], \
+        yerrb_n[0][::1000]
 
     # run on noisy and noise free
-    mcmc_fit(x, y, yerr, p_init, plims, sid, RESULTS_DIR, trths,
-	     burnin=burnin, nwalkers=nwalkers, nruns=nruns, full_run=full_run)
-    mcmc_fit(x_n, y_n, yerr_n, p_init_n, plims_n, "{0}_n".format(sid),
-             RESULTS_DIR, trths, burnin=burnin, nwalkers=nwalkers, nruns=nruns,
-             full_run=full_run)
+#     mcmc_fit(xb[:2], yb[:2], yerrb[:2], p_init, p_max, sid, RESULTS_DIR,
+    mcmc_fit(xb[0], yb[0], yerrb[0], p_init, p_max, sid, RESULTS_DIR,
+             truths=trths, burnin=burnin, nwalkers=nwalkers, nruns=nruns,
+             full_run=full_run, diff_threshold=.5, n_independent=1000)
+#     mcmc_fit(xb_n[:2], yb_n[:2], yerrb_n[:2], p_init_n, p_max,
+    mcmc_fit(xb_n[0], yb_n[0], yerrb_n[0], p_init_n, p_max,
+             "{0}_n".format(sid), RESULTS_DIR, truths=trths, burnin=burnin,
+             nwalkers=nwalkers, nruns=nruns, full_run=full_run,
+             diff_threshold=.5, n_independent=1000)
 
 if __name__ == "__main__":
 
-    pool = Pool()
-    results = pool.map(recover, range(333))
+#     pool = Pool()
+#     results = pool.map(recover, range(333))
+
+    recover(0)
 
 #     for i in range(333):
 # 	    recover(i)
