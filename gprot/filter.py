4083a26f93f0f6139a822d88dee22fc8f3c86ab2
Timothy Morton <tim.morton@gmail.com>
2016-11-29 16:22:12 -0500

    added missing file

---
+++ gprot/filter.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def sigma_clip(x, y, yerr, nsigma):
+    med = np.median(y)
+    std = (sum((med - y)**2)/float(len(y)))**.5
+    m = np.abs(y - med) > (nsigma * std)
+
+    return x[~m], y[~m], yerr[~m]
