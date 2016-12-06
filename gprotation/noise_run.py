e537cbeb0aaa40b7c89ddf0f4964e40c589121d9
RuthAngus <ruth.angus@astro.ox.ac.uk>
2016-12-06 23:00:10 +0000

    adding noisy_sinusoid

--- gprotation/noise_run.py
+++ gprotation/noise_run.py
@@ -13,11 +13,22 @@ def noise_fit(id, RESULTS_DIR):
     y = np.random.randn(len(x)) * 1e-5
     yerr = np.ones_like(y) * 1e-5
 
-    fit = gp.fit(x, y, yerr, id, RESULTS_DIR, lc_size=201)
+    fit = gp.fit(x, y, yerr, id, RESULTS_DIR)
     fit.gp_fit(burnin=1000, nwalkers=16, nruns=5, full_run=1000, nsets=1)
+
+def noisy_sinusoid(period, amp, id, RESULTS_DIR):
+
+    gap_days = .02043365
+    x = np.arange(0, 200, gap_days)
+    y = amp*np.sin(2*np.pi*(1./period)*x)
+    y += np.random.randn(len(x)) * 1e-5
+    yerr = np.ones_like(y) * 1e-5
+
+    fit = gp.fit(x, y, yerr, id, RESULTS_DIR)
+    fit.gp_fit(burnin=3000, nwalkers=16, nruns=5, full_run=1000, nsets=1)
 
 
 if __name__ == "__main__":
-    id = 0
     RESULTS_DIR = "noise"
-    noise_fit(id, RESULTS_DIR)
+#     noise_fit(0, RESULTS_DIR)
+    noisy_sinusoid(10, 1e-3, 1, RESULTS_DIR)
