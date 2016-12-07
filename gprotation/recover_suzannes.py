e45f5b4c0691d1dea0f0352528d8a8897cbfd89c
RuthAngus <ruth.angus@astro.ox.ac.uk>
2016-12-07 23:15:32 +0000

    changing the run parameters

--- gprotation/recover_suzannes.py
+++ gprotation/recover_suzannes.py
@@ -73,13 +73,13 @@ def gp_fit(x, y, yerr, sid, RESULTS_DIR):
 
     # fast settings
 #     burnin, nwalkers, nruns, full_run = 500, 12, 3, 100
-    burnin, nwalkers, nruns, full_run = 2, 12, 5, 50
+#     burnin, nwalkers, nruns, full_run = 2, 12, 5, 50
 #     xb[0], yb[0], yerrb[0] = xb[0][::100], yb[0][::100], yerrb[0][::100]
 
     trths = [None, None, None, None, None]
 #     trths = [None, None, None, None, np.log(truths.P_MIN.values[m][i])]
-#     mcmc_fit(xb[:2], yb[:2], yerrb[:2], p_init, p_max, sid, RESULTS_DIR,
-    mcmc_fit(xb[0], yb[0], yerrb[0], p_init, p_max, sid, RESULTS_DIR,
+#     mcmc_fit(xb[0], yb[0], yerrb[0], p_init, p_max, sid, RESULTS_DIR,
+    mcmc_fit(xb[:2], yb[:2], yerrb[:2], p_init, p_max, sid, RESULTS_DIR,
              truths=trths, burnin=burnin, nwalkers=nwalkers, nruns=nruns,
              full_run=full_run, diff_threshold=.5, n_independent=1000)
 
